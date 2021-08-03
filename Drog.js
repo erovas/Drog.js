989b95f324d95f1c14436fbf9eeebf7ebbff268a
erovas <emarovas2@gmail.com>
2021-08-03 23:59:26 +0200

    initial upload

---
+++ Drog.js
@@ -0,0 +1,116 @@
+/*!
+ * Drog.js v1.0.0
+ * [Back-compatibility: IE11+]
+ * Copyright (c) 2021, Emanuel Rojas Vásquez
+ * BSD 3-Clause License
+ * https://github.com/erovas/Drog.js
+ */
+(function(window, document){
+
+    if(window.Drog)
+        return console.error('Drog.js has already been defined');
+
+    let Xi = 0, Yi = 0, Xf = 0, Yf = 0, Xt = '-x', Yt = '-y',
+        mdown = 'mousedown', tstart = 'touchstart',
+        mmove = 'mousemove', tmove = 'touchmove',
+        mup = 'mouseup', tend = 'touchend',
+        father = '-f', passive = { passive: false },
+        isDrog = '-d', data = '[data-drog]', elmnt;
+
+    function addEvent(element, event, callback, opt){
+        element.addEventListener(event, callback, opt || false);
+    }
+
+    function removeEvent(element, event, callback, opt){
+        element.removeEventListener(event, callback, opt || false);
+    }    
+
+    function on(element){
+
+        if(element[isDrog])
+            return;
+
+        let target = element.querySelector(data) || element;
+
+        // save reference original element into target
+        target[father] = element;
+
+        // Save element position
+        element[Xt] = 0;
+        element[Yt] = 0;
+
+        element.style.zIndex = 10;
+
+        // signing element
+        element[isDrog] = true;
+
+        addEvent(target, mdown, drogInit);
+        addEvent(target, tstart, drogInit, passive);
+    }
+
+    function off(element){
+
+        if(!element[isDrog])
+            return;        
+
+        let target = element.querySelector(data) || element;
+
+        element.style.zIndex = '';
+        element.style.transform = '';
+
+        // deleting references
+        target[father] = null;
+
+        // unsigning element
+        element[isDrog] = false;
+
+        removeEvent(target, mdown, drogInit);
+        removeEvent(target, tstart, drogInit);
+    }
+
+    function drogInit(e){
+        elmnt = this[father];
+
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        Xi = e.clientX || e.touches[0].clientX;
+        Yi = e.clientY || e.touches[0].clientY;
+
+        // call a function whenever the cursor moves:
+        addEvent(document, mmove, drogMove);
+        addEvent(document, tmove, drogMove, passive);
+
+        // call a function when cursor/touch up/end
+        addEvent(document, mup, drogEnd);
+        addEvent(document, tend, drogEnd, passive);
+    }
+
+    function drogMove(e){
+        e.preventDefault();
+        // calculate the new cursor position:
+        Xf = e.clientX || e.touches[0].clientX;
+        Yf = e.clientY || e.touches[0].clientY;
+
+        elmnt[Xt] -= Xi - Xf;
+        elmnt[Yt] -= Yi - Yf;
+
+        Xi = Xf;
+        Yi = Yf;
+
+        elmnt.style.transform = 'translate(' + elmnt[Xt] + 'px,' + elmnt[Yt] + 'px)';
+    }
+
+    function drogEnd(){
+        // stop moving when mouse/touch is released:
+        removeEvent(document, mmove, drogMove);
+        removeEvent(document, tmove, drogMove, passive);
+        removeEvent(document, mup, drogEnd);
+        removeEvent(document, tend, drogEnd, passive);
+    }
+
+    window.Drog = {
+        on: on,
+        off: off
+    }
+
+})(window, document);
--- README.md
+++ README.md
@@ -1,2 +1,154 @@
 # Drog.js
 Drog.js is a fast, cross-browser and mobile-compatible drag and drop library for JavaScript
+
+## Compatibility
+- Works with all modern browsers from IE11+.
+
+## How to use it?
+
+Import the Drog.js JavaScript library wherever you want into the document before using it.
+
+``` html
+<script src="Drog.min.js"></script>
+<script>
+    Drog.on(document.getElementById("element"))
+</script>
+```
+
+or
+
+``` html
+<script defer src="Drog.min.js"></script>
+<script defer src="otherScript.js"></script>
+```
+
+or
+
+``` html
+<script type="module" src="Drog.min.js"></script>
+<script type="module" src="otherScript.js"></script>
+```
+
+or
+
+``` html
+<script type="module">
+    import "Drog.min.js";
+    Drog.on(document.getElementById("element"))
+</script>
+```
+
+Define an internal element with the [data-drog] property, to serve as a drag point:
+
+``` html
+<div id="element">
+    <div data-drog>
+        <span>Click here to move</span>
+    </div>
+    <p>Move</p>
+    <p>this</p>
+    <p>ELEMENT</p>
+</div>
+```
+
+or so, to drag the element with any point of itself (see demo)
+
+<div id="element">
+    <div>
+        <span>Click to</span>
+    </div>
+    <p>Move</p>
+    <p>this</p>
+    <p>ELEMENT</p>
+</div>
+```
+
+## Demo
+
+https://erovas.github.io/Drog.js/
+
+## Authors
+
+* **Emanuel Rojas Vásquez** - *Initial work* - [erovas](https://github.com/erovas)
+
+## License
+
+This project is licensed under the BSD 3-Clause License - see the [LICENSE](https://github.com/erovas/Drog.js/blob/main/LICENSE) file for details.
+
+# Spanish - Español
+
+# Drog.js
+Drog.js es un ligero, cross-browser y movil-compatible libreria de drag and drop JavaScript
+
+## Compatibilidad
+- Funciona con todos los navegadores modernos desde IE11+.
+
+## ¿Cómo utilizarlo?
+Importar la libreria Drog.js en cualquier parte dentro del documento antes de utilizarlo.
+
+``` html
+<script src="Drog.min.js"></script>
+<script>
+    Drog.on(document.getElementById("element"))
+</script>
+```
+
+o
+
+``` html
+<script defer src="Drog.min.js"></script>
+<script defer src="otherScript.js"></script>
+```
+
+o
+
+``` html
+<script type="module" src="Drog.min.js"></script>
+<script type="module" src="otherScript.js"></script>
+```
+
+o
+
+``` html
+<script type="module">
+    import "Drog.min.js";
+    Drog.on(document.getElementById("element"))
+</script>
+```
+
+Define un elemento interno con la propiedad [data-drog], para servir como punto de arrastre:
+
+``` html
+<div id="element">
+    <div data-drog>
+        <span>Click here to move</span>
+    </div>
+    <p>Move</p>
+    <p>this</p>
+    <p>ELEMENT</p>
+</div>
+```
+
+o de esta forma, para arrastrar el elemento desde cualquier punto de el mismo (ver demo)
+
+<div id="element">
+    <div>
+        <span>Click to</span>
+    </div>
+    <p>Move</p>
+    <p>this</p>
+    <p>ELEMENT</p>
+</div>
+```
+
+## Demo
+
+https://erovas.github.io/Drog.js/
+
+## Autores
+
+* **Emanuel Rojas Vásquez** - *Initial work* - [erovas](https://github.com/erovas)
+
+## Licencia
+
+Este proyecto está licenciado bajo Licencia BSD 3-Clause - ver el archivo [LICENCIA](https://github.com/erovas/Drog.js/blob/main/LICENSE) para mas detalles.
